0d1f8a5b576d03ad856d0bf21e7d2ed91aad87f0
Ajul K Jose <57012008+ajulkjose246@users.noreply.github.com>
2024-04-04 21:36:05 +0530

    Update pyro4servernew.py

--- 16.Pyro4 server simple/pyro4servernew.py
+++ 16.Pyro4 server simple/pyro4servernew.py
@@ -1,12 +1,10 @@
 import Pyro4
-class Myserver(object):
-    @Pyro4.expose
-    def greet(self,name):
-        return "Hello, {}!".format(name)
-daemon=Pyro4.Daemon()
-uri=daemon.register(Myserver)
-print("server URI:",uri)
-ns=Pyro4.locateNS()
-ns.register("myserver", uri)
-print("server ready.")
-daemon.requestLoop()
+
+@Pyro4.expose
+class MyServer:
+    def greet(self, name):
+        return f"Hello, {name}!"
+Pyro4.Daemon.serveSimple({MyServer: "myserver"},ns=True)
+
+# python -m Pyro4.naming
+# run server and client
